e1db6149ac640a00d47808e9742af3116b6bbb12
Sasha Czerniawski <sashacandoit@gmail.com>
2022-10-16 10:06:20 -0400

    loaded initial data, connected to database and exported the client object

--- db.js
+++ db.js
@@ -1,2 +1,22 @@
 /** Database setup for BizTime. */
 
+const { Client } = require("pg");
+
+let DB_URI;
+
+if (process.env.NODE_ENV === "test") {
+    DB_URI = "postgresql:///users_test";
+    //Lets us use a separate database for testing
+} else {
+    DB_URI = "postgresql:///users";
+}
+
+let db = new Client({
+    connectionString: DB_URI
+    //tells node where the database is so it can connect to it 
+});
+
+db.connect();
+//starts up the connection
+
+module.exports = db;
