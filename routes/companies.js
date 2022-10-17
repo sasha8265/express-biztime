9dc78ffb033952a6ad283d0ff6bb3a3f9a9f4f2f
Sasha Czerniawski <sashacandoit@gmail.com>
2022-10-17 07:27:22 -0400

    PATCH route added for companies

--- routes/companies.js
+++ routes/companies.js
@@ -37,5 +37,19 @@ router.post("/", async (req, res, next) => {
     }
 });
 
+router.patch('/:code', async (req, res, next) => {
+    try {
+        const { code } = req.params;
+        const { name, description } = req.body;
+        const results = await db.query('UPDATE companies SET name=$1, description=$2 WHERE code=$3 RETURNING code, name, description', [name, description, code])
+        if (results.rows.length === 0) {
+            throw new ExpressError(`Can't find company with code of ${code}`, 404)
+        }
+        return res.json({ company: results.rows[0] })
+    } catch (e) {
+        return next(e)
+    }
+})
+
 
 module.exports = router;
