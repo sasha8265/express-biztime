fa716ed35bad0f71ffcf42a714bc0fb003367ed7
Sasha Czerniawski <sashacandoit@gmail.com>
2022-10-17 07:18:38 -0400

    POST in companies route added

--- routes/companies.js
+++ routes/companies.js
@@ -25,5 +25,17 @@ router.get('/:code', async (req, res, next) => {
     }
 })
 
+router.post("/", async (req, res, next) => {
+    try {
+        const { code, name, description } = req.body;
+        const result = await db.query(
+            `INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING code, name, description`, [code, name, description]
+        );
+        return res.json({company: result.rows[0]})
+    } catch (e) {
+        return next(e)
+    }
+});
+
 
 module.exports = router;
